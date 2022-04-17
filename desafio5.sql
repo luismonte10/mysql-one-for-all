c9d370796eb2bc7cbd3b5f09705d4f46b7825582
Luis Monte <luismonte.l560@gmail.com>
2022-04-17 20:31:29 -0300

    Crie uma QUERY que possua duas colunas: cancao, reproducoes. Onde reproducoes contém a quantidas de reproduções da musica.

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT 
+    mus.music_name AS cancao,
+    COUNT(mus.music_name) AS reproducoes
+FROM
+    SpotifyClone.musics AS mus
+        INNER JOIN
+    history AS his ON his.music_id = mus.music_id
+GROUP BY mus.music_name
+ORDER BY COUNT(mus.music_name) DESC, mus.music_name ASC
+LIMIT 2;
