0e9b3d394d12a66a7516defc6d7159c3b407cd7c
Luis Monte <luismonte.l560@gmail.com>
2022-05-02 14:49:32 -0300

    Alteração no group by do req 07.

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT 
-	art.artist_name AS artista,
+		art.artist_name AS artista,
     alb.album_name AS album,
     COUNT(fol.artist_id) AS seguidores
 FROM
@@ -8,5 +8,5 @@ FROM
 	SpotifyClone.artists AS art ON art.artist_id = alb.artist_id
 		INNER JOIN
 	SpotifyClone.follows AS fol ON fol.artist_id = alb.artist_id
-GROUP BY art.artist_name, alb.album_name
+GROUP BY alb.album_id
 ORDER BY COUNT(fol.artist_id) DESC, art.artist_name, alb.album_name;
